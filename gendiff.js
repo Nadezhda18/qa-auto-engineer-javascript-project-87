e5aa5cf8a866ae2bba9a29dfafe33bf4291e7ea1
Nadya <msyoo1682@gmail.com>
2025-07-18 21:27:33 +0300

    gendiff.js

---
+++ gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const commander = require('commander');
+const program = new commander.Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'display help for command')
+  .action(() => {
+    console.log(program.help())
+  })
+
+program.parse(process.argv)
