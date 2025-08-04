a6d524a32b8de17695b26ca7eddc933df4cbdf30
Nadya <msyoo1682@gmail.com>
2025-08-04 21:33:21 +0300

    gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -4,9 +4,11 @@ const program = new commander.Command();
 
 program
   .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .option('-h, --help', 'display help for command')
+  .option('-f, --format', '<type>  output format')
   .action(() => {
     console.log(program.help())
   })
